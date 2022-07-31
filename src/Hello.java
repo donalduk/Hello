b7805ae9050a1ccff49cc410b16d063c799ba2af
donald <donaldyy@hotmail.com>
2022-07-31 17:51:31 +0100

    hello

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        return;
+
+
+    }
+
+    // TODO implement echo
+    public void echo() {
+
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+    private String lName;
+    private String firstName;
+
+    public String getLName() {
+        return lName;
+    }
+
+    public void setLName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < ; i++) {
+            
+        }
+        if (args == null) {
+            
+        }
+    }
+}
